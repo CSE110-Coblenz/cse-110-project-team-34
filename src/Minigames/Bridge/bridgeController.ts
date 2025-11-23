1a4645448adcff6f9e33c791ff70890601e51359
ahp002 <aliciap.ap6@gmail.com>
2025-11-23 15:26:45 -0800

    prevent states from being chosen that are next to each other and prevent hawaii and alaska from being chosen

--- src/Minigames/Bridge/bridgeController.ts
+++ src/Minigames/Bridge/bridgeController.ts
@@ -1,39 +1,70 @@
+// Handles user input, updates model, updates view
+
 import { findShortestBridgeSize } from '../../utils/bridgeMinigameUtils';
-import { stateAdjacencyList } from '../../common/USMapData';
+import { stateAdjacencyList, stateNames } from '../../common/USMapData';
 import { BridgeView } from '../Bridge/bridgeView';
 import { BridgeModel } from '../Bridge/bridgeModel';
+import {BaseGameModel} from '../../common/BaseGameModel';
 
 export class BridgeController {
     private view: BridgeView;
     private model: BridgeModel;
+    private baseModel: BaseGameModel;
     private currentAnswer: number = 0;
     private stateA: string = '';
     private stateB: string = '';
+    private stateAName: string = '';
+    private stateBName: string = '';
 
-    constructor(view: BridgeView, model: BridgeModel) {
+
+    constructor(view: BridgeView, model: BridgeModel, baseModel: BaseGameModel) {
         this.view = view;
         this.model = model;
+        this.baseModel = baseModel;
 
         // Register callback for when user submits a guess
         this.view.onGuessSubmitted((guess: number) => this.checkGuess(guess));
     }
+    
+    pauseClock() {
+        this.baseModel.pauseGameClock();
+    }
+
+    resumeClock() {
+        this.baseModel.resumeGameClock();
+    }
+
 
     /** Starts a new minigame round */
     startMinigame() {
         const states = Array.from(stateAdjacencyList.keys());
 
-        // Pick two random states (avoid same state twice)
-        this.stateA = states[Math.floor(Math.random() * states.length)];
+        let a: string;
+        let b: string;
+
         do {
-            this.stateB = states[Math.floor(Math.random() * states.length)];
-        } while (this.stateB === this.stateA);
+            a = states[Math.floor(Math.random() * states.length)];
+        } while (a === "hi" || a === "ak"); // block Hawaii + Alaska
 
-        // Compute answer using the util
-        this.currentAnswer = findShortestBridgeSize(this.stateA, this.stateB);
+        do {
+            b = states[Math.floor(Math.random() * states.length)];
+        } while (
+            b === a || b === "hi" || b === "ak" ||
+            stateAdjacencyList.get(a)?.has(b) // block pairs that are neighbors
+        );
 
-        // Show question in the view
-        this.view.showMinigameQuestion(this.stateA, this.stateB);
+        this.stateA = a;
+        this.stateB = b;
+
+        this.currentAnswer = findShortestBridgeSize(a, b);
+
+        this.stateAName = stateNames.get(a) || a;
+        this.stateBName = stateNames.get(b) || b;
+
+        // this.baseModel.pauseGameClock();
+        this.view.showMinigameQuestion(this.stateAName, this.stateBName);
     }
+
 
     /** Checks the player's guess */
     private checkGuess(userGuess: number) {
@@ -45,5 +76,6 @@ export class BridgeController {
 
         // Show result in the view
         this.view.showMinigameResult(isCorrect, this.currentAnswer);
+        this.resumeClock();
     }
 }
